23064fa8140d53cf94abdb7cc38409985bbb66b2
ramaramx <rambudiarto@gmail.com>
2022-09-25 10:54:12 +0700

    metaarena video

--- video.js
+++ video.js
@@ -1,4 +1,46 @@
-/* global AFRAME */
+// custom videotron geometry
+AFRAME.registerComponent('a-shape', {
+  schema: {
+    // points will be parsed locally to a set of tuples, so this will become
+      // [0,0],[3,0],[5,3],[3,5]
+    points: {type: 'array', default: 
+             [3,-2, -3,-2, -2,2, 2,2]
+             // [0,0 , 1,1 , 2,2]
+            }
+  },
+  init() {
+    if (this.data.points % 2 !== 0) {
+      console.warn("supplied odd number of points, will ignore last point")
+    }
+    
+    let points = [];
+    for (let i = 0; i < this.data.points.length; i+=2) {
+      points.push(new THREE.Vector2(this.data.points[i], this.data.points[i+1]))
+    }
+
+    for (var i = 0; i < points.length; i++) {
+      points[i].multiplyScalar(0.25);
+    }
+    
+    var shape = new THREE.Shape(points);
+
+    var geometry = new THREE.ShapeGeometry(shape);
+    // var video = document.querySelector('#example-vid');
+    // var texture = new THREE.VideoTexture( video )
+    
+    var material = new THREE.MeshBasicMaterial({
+      color: 'white',
+      shader: 'flat',
+      side: 'double',
+      transparent: true,
+      // map: texture,
+    });
+    var mesh = new THREE.Mesh(geometry, material);
+    this.el.object3D.add(mesh);
+  },
+});
+
+/* hide on play */
 AFRAME.registerComponent('hide-on-play',{
     schema:{type:'selector'},
     init:function(){
@@ -26,7 +68,7 @@ AFRAME.registerComponent('hide-on-play',{
     }
   });
 
-  /* global AFRAME */
+  /* click play video */
 AFRAME.registerComponent('play-on-click',{
     init:function(){
       this.onClick=this.onClick.bind(this);
